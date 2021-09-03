4d46eebe003422998d01b266852b1a81fab25d97
parksunyoung <iss002@naver.com>
2021-09-03 17:28:45 +0900

    project06 create.

---
+++ src/main/Interface_Member.java
@@ -0,0 +1,8 @@
+package main;
+
+public interface Interface_Member {
+	public void getList();
+	public void getAdd();
+	public void getDelete();
+	public void getModify();
+}
---
+++ src/main/MainClass.java
@@ -0,0 +1,35 @@
+package main;
+
+import java.util.Scanner;
+
+public class MainClass {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int input = 0;
+		
+		while(true) {
+			System.out.println("1.박상진");
+			System.out.println("2.김여진");
+			System.out.println("3.이동호");
+			System.out.println("4.김형석");
+			System.out.println("5.문지은");
+			System.out.println("6.박선영");
+			
+			input = sc.nextInt(); sc.nextLine();
+			
+			switch (input) {
+			case 1: break;
+			case 2: break;
+			case 3: break;
+			case 4: break;
+			case 5: break;
+			case 6: break;
+			
+			case 7: System.exit(0); break;
+			default:
+				System.out.println("only 1~7.");
+				break;
+			}
+		}
+	}
+}
